dfec260eb9c6fc4dcb70cf8ecaaa8aa7f5e6d765
Mengchea <sarousomengchea@gmail.com>
2021-04-09 19:41:31 +0700

    Added some themes

--- theme_switcher/src/main/java/com/mengchea/theme_switcher/ThemeSwitcher.kt
+++ theme_switcher/src/main/java/com/mengchea/theme_switcher/ThemeSwitcher.kt
@@ -1,16 +1,22 @@
 package com.mengchea.theme_switcher
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import androidx.annotation.DrawableRes
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.content.res.ResourcesCompat
-
+import androidx.core.graphics.drawable.DrawableCompat
 
 class ThemeSwitcher : SwitchCompat {
 
     internal var context: Context
     private var themeStyle: Int = 1
+    private var trackDayColor: Int = 0
+    private var trackNightColor: Int = 0
 
     constructor(context: Context) : super(context) {
         this.context = context
@@ -18,9 +24,13 @@ class ThemeSwitcher : SwitchCompat {
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs){
         this.context = context
-        val a: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.ThemeSwitcher)
-        themeStyle = a.getInt(R.styleable.ThemeSwitcher_themeStyle, 1)
-        a.recycle()
+        val style: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.ThemeSwitcher)
+        themeStyle = style.getInt(R.styleable.ThemeSwitcher_themeStyle, 1)
+        val defDayColor = ResourcesCompat.getColor(resources, R.color.color_light, null)
+        val defNightColor = ResourcesCompat.getColor(resources, R.color.color_dark, null)
+        trackDayColor = style.getColor(R.styleable.ThemeSwitcher_trackDayColor, defDayColor)
+        trackNightColor = style.getColor(R.styleable.ThemeSwitcher_trackNightColor, defNightColor)
+        style.recycle()
         initTheme()
     }
 
@@ -30,13 +40,26 @@ class ThemeSwitcher : SwitchCompat {
 
     private fun initTheme() {
         when (themeStyle) {
-            1 -> {
-                thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.thumb_switcher, null)
-            }
-            2 -> {
-                thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.thumb2_switcher, null)
-            }
+            1 -> thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.thumb_switcher, null)
+            2 -> thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.thumb2_switcher, null)
         }
-        trackDrawable = ResourcesCompat.getDrawable (resources, R.drawable.track_switcher, null)
+
+        trackDrawable = setColor(context, R.drawable.track_switcher)
+
+        background = null
+
     }
+
+    private fun setColor(context: Context, @DrawableRes resId: Int): Drawable {
+        val drawable = AppCompatResources.getDrawable(context, resId)
+        val drawableCompat = DrawableCompat.unwrap<Drawable>(drawable!!)
+        val colorState = ColorStateList(
+                arrayOf(intArrayOf(-android.R.attr.state_checked),
+                        intArrayOf(android.R.attr.state_checked)
+                ), intArrayOf(trackDayColor, trackNightColor)
+        )
+        DrawableCompat.setTintList(drawableCompat, colorState)
+        return drawable
+    }
+
 }
